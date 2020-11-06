8f931cc6a2ee54a809d2c0f1520f6647627aed8b
jaywarfield <jay.warfield@us.ibm.com>
2020-11-06 10:56:13 -0500

    Adding structure for 3 versions

--- src/main/webapp/js/diagramly/sidebar/Sidebar.js
+++ src/main/webapp/js/diagramly/sidebar/Sidebar.js
@@ -487,6 +487,8 @@
             			          {title: 'Citrix', id: 'citrix', image: IMAGE_PATH + '/sidebar-citrix.png'},
             			          {title: 'Google Cloud Platform', id: 'gcp2', image: IMAGE_PATH + '/sidebar-gcp2.png'},
             			          {title: 'IBM 1.0', id: 'ibm', image: IMAGE_PATH + '/sidebar-ibm.png'},
+            			          {title: 'IBM 1.1', id: 'ibm1', image: IMAGE_PATH + '/sidebar-ibm1.png'},
+            			          {title: 'IBM 2.0', id: 'ibm2', image: IMAGE_PATH + '/sidebar-ibm2.png'},
             			          {title: 'Kubernetes', id: 'kubernetes', image: IMAGE_PATH + '/sidebar-kubernetes.png'},
             			          {title: 'Network', id: 'network', image: IMAGE_PATH + '/sidebar-network.png'},
             			          {title: 'Office', id: 'office', image: IMAGE_PATH + '/sidebar-office.png'},
