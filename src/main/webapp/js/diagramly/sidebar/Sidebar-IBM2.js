4a88dca2758465ca48594c8fdb2bb6114f146425
jaywarfield <jay.warfield@us.ibm.com>
2021-01-03 18:41:41 -0500

    Added initial groups

--- src/main/webapp/js/diagramly/sidebar/Sidebar-IBM2.js
+++ src/main/webapp/js/diagramly/sidebar/Sidebar-IBM2.js
@@ -1,28 +1,19 @@
 (function()
 {
-	Sidebar.prototype.addIBM2Palette = function()
+	Sidebar.prototype.addIBM2MondrianPalette = function()
 	{
-		var d = 60;
+		var w = 240;
+		var h = 48;
+		var gn = 'mxgraph.ibm2';
 		var dt = 'ibm ';
-		var sb = this;
-		var s = 'shape=mxgraph.ibm2mondrian.base;shapeType=group;html=1;whiteSpace=wrap;fontFamily=IBM Plex Sans;fontColor=#000000;fontSize=14;verticalAlign=middle;align=left;spacing=8;spacingLeft=12;spacingRight=16;spacingTop=0;spacingBottom=0;metaEdit=0;strokeWidth=1;container=1;collapsible=0;recursiveResize=0;';
-		
-		// Adds IBM shapes
-		this.setCurrentSearchEntryLibrary('ibm2', 'ibm2Groups');
-		this.addIBM2GroupsPalette(d, dt, sb, s);
-		this.setCurrentSearchEntryLibrary();
-	};
 
-	Sidebar.prototype.addIBM2GroupsPalette = function(d, dt, sb, s)
-	{
-		var x = 240;
-		var y = 160;
-
-		var cloudColor = "colorFamily=blue"
+		var cloudColor = "colorFamily=blue;"
 		var accountColor = "colorFamily=purple;"
 		var locationColor = "colorFamily=gray;"
 		var networkColor = "colorFamily=teal;"
 		var securityColor = "colorFamily=red;"
+
+		this.setCurrentSearchEntryLibrary('ibm2', 'ibm2Groups');
 
 		var fns = [
 			 // General groups
@@ -32,18 +23,23 @@
 			 this.createVertexTemplateEntry(s + locationColor + 'image=devices;', x, y, 'Zone', 'Zone', true, null, null),
 			 this.createVertexTemplateEntry(s + cloudColor + 'image=events;', x, y, 'Public Network', 'Public Network', true, null, null),
 			 this.createVertexTemplateEntry(s + cloudColor + 'image=enterprise;', x, y, 'Enterprise Network', 'Enterprise Network', true, null, null),
+			 this.createVertexTemplateEntry(s + accountColor + 'image=credentials;', x, y, 'Access Group', 'Access Group', true, null, null),
+			 this.createVertexTemplateEntry(s + accountColor + 'image=user;', x, y, 'Account Group', 'Account Group', true, null, null),
+			 this.createVertexTemplateEntry(s + accountColor + 'image=collaborate;', x, y, 'Resource Group', 'Account Group', true, null, null),
 
 			 // VPC groups
 			 this.createVertexTemplateEntry(s + cloudColor + 'image=virtual-private-cloud;', x, y, 'VPC', 'VPC', true, null, null),
 			 this.createVertexTemplateEntry(s + networkColor + 'image=virtual-private-cloud;', x, y, 'Subnet', 'Subnet', true, null, null),
 		];
-			   	
-   		this.addPalette('ibm2Groups', 'IBM 2.0 / Cloud / Groups', false, mxUtils.bind(this, function(content)
-	    {
+
+		this.addPalette('ibm2Groups', 'IBM 2.0 / Cloud / Groups', false, mxUtils.bind(this, function(content)
+		{
 			for (var i = 0; i < fns.length; i++)
 			{
 				content.appendChild(fns[i](content));
 			}
 		}));
+
+		this.setCurrentSearchEntryLibrary();
 	};
 })();
