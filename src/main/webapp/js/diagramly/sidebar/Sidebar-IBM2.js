202799fa4809fc9d6e963872b35105d9a252c72f
jaywarfield <jay.warfield@us.ibm.com>
2021-01-03 19:33:43 -0500

    Added initial groups

--- src/main/webapp/js/diagramly/sidebar/Sidebar-IBM2.js
+++ src/main/webapp/js/diagramly/sidebar/Sidebar-IBM2.js
@@ -3,7 +3,7 @@
 	Sidebar.prototype.addIBM2Palette = function()
 	{
 		var w = 240;
-		var h = 48;
+		var h = 160;
 		var gn = 'mxgraph.ibm2';
 		var dt = 'ibm ';
 
@@ -17,30 +17,48 @@
 
 		this.setCurrentSearchEntryLibrary('ibm2', 'ibm2Groups');
 
-		var fns = [
+		//var fns = [
 			 // General groups
-			 this.createVertexTemplateEntry(style_text + cloudColor + 'image=ibm-cloud;', w, h, 'IBM Cloud', 'IBM Cloud', true, null, null),
-			 this.createVertexTemplateEntry(style_text + locationColor + 'image=flag;', w, h, 'Region', 'Region', true, null, null),
-			 this.createVertexTemplateEntry(style_text + locationColor + 'image=location;', w, h, 'Region', 'Region', true, null, null),
-			 this.createVertexTemplateEntry(style_text + locationColor + 'image=devices;', w, h, 'Zone', 'Zone', true, null, null),
-			 this.createVertexTemplateEntry(style_text + cloudColor + 'image=events;', w, h, 'Public Network', 'Public Network', true, null, null),
-			 this.createVertexTemplateEntry(style_text + cloudColor + 'image=enterprise;', w, h, 'Enterprise Network', 'Enterprise Network', true, null, null),
-			 this.createVertexTemplateEntry(style_text + accountColor + 'image=credentials;', w, h, 'Access Group', 'Access Group', true, null, null),
-			 this.createVertexTemplateEntry(style_text + accountColor + 'image=user;', w, h, 'Account Group', 'Account Group', true, null, null),
-			 this.createVertexTemplateEntry(style_text + accountColor + 'image=collaborate;', w, h, 'Resource Group', 'Account Group', true, null, null),
+			 //this.createVertexTemplateEntry(style_text + cloudColor + 'image=ibm-cloud;', w, h, 'IBM Cloud', 'IBM Cloud', true, null, null),
+			 //this.createVertexTemplateEntry(style_text + locationColor + 'image=flag;', w, h, 'Region', 'Region', true, null, null),
+			 //this.createVertexTemplateEntry(style_text + locationColor + 'image=location;', w, h, 'Region', 'Region', true, null, null),
+			 //this.createVertexTemplateEntry(style_text + locationColor + 'image=devices;', w, h, 'Zone', 'Zone', true, null, null),
+			 //this.createVertexTemplateEntry(style_text + cloudColor + 'image=events;', w, h, 'Public Network', 'Public Network', true, null, null),
+			 //this.createVertexTemplateEntry(style_text + cloudColor + 'image=enterprise;', w, h, 'Enterprise Network', 'Enterprise Network', true, null, null),
+			 //this.createVertexTemplateEntry(style_text + accountColor + 'image=credentials;', w, h, 'Access Group', 'Access Group', true, null, null),
+			 //this.createVertexTemplateEntry(style_text + accountColor + 'image=user;', w, h, 'Account Group', 'Account Group', true, null, null),
+			 //this.createVertexTemplateEntry(style_text + accountColor + 'image=collaborate;', w, h, 'Resource Group', 'Account Group', true, null, null),
 
 			 // VPC groups
-			 this.createVertexTemplateEntry(style_text + cloudColor + 'image=virtual-private-cloud;', w, h, 'VPC', 'VPC', true, null, null),
-			 this.createVertexTemplateEntry(style_text + networkColor + 'image=virtual-private-cloud;', w, h, 'Subnet', 'Subnet', true, null, null),
-		];
+			 //this.createVertexTemplateEntry(style_text + cloudColor + 'image=virtual-private-cloud;', w, h, 'VPC', 'VPC', true, null, null),
+			 //this.createVertexTemplateEntry(style_text + networkColor + 'image=subnet;', w, h, 'Subnet', 'Subnet', true, null, null),
+			 //this.createVertexTemplateEntry(style_text + securityColor + 'image=security;', w, h, 'Subnet', 'Subnet', true, null, null),
+		//];
 
-		this.addPalette('ibm2Groups', 'IBM 2.0 / Cloud / Groups', false, mxUtils.bind(this, function(content)
-		{
-			for (var i = 0; i < fns.length; i++)
-			{
-				content.appendChild(fns[i](content));
-			}
-		}));
+		//this.addPalette('ibm2Groups', 'IBM 2.0 / Cloud / Groups', false, mxUtils.bind(this, function(content)
+		//{
+		//	for (var i = 0; i < fns.length; i++)
+		//	{
+		//		content.appendChild(fns[i](content));
+		//	}
+		//}));
+
+		this.addPaletteFunctions('ibm2', 'IBM 2.0 / Cloud / Groups', false,
+		[
+			this.addEntry(dt + 'tn', function()
+		   	{
+			    var bg = new mxCell('', new mxGeometry(0, 0, w, h), style_text + cloudColor);
+		    		bg.vertex = true;
+		    		bg.setValue(mxUtils.createXmlDocument().createElement('object'));
+				bg.setAttribute('placeholders', '1');
+				bg.setAttribute('label', '<B>%Element-Name%</B><BR><font style=\'font-size: 12px\'>%Element-ID%</font>');
+                		bg.setAttribute('Element-ID', 'Element-ID');
+				bg.setAttribute('Element-Name', 'IBM Cloud');
+				bg.setAttribute('Icon-Name', 'ibm-cloud');
+			    
+			   	return sb.createVertexTemplateFromCells([bg], bg.geometry.width, bg.geometry.height, 'BASE');
+			})				
+		]);
 
 		this.setCurrentSearchEntryLibrary();
 	};
