9b4a9f0ebe91761fe49787867a34ecf1fa655aec
jaywarfield <jay.warfield@us.ibm.com>
2021-01-08 18:13:18 -0500

    minor ibm1.1 cleanup

--- src/main/webapp/js/diagramly/sidebar/Sidebar.js
+++ src/main/webapp/js/diagramly/sidebar/Sidebar.js
@@ -124,7 +124,7 @@
 	                                   {id: 'bpmn', prefix: 'bpmn', libs: [''/*prefix is library*/, 'Gateways', 'Events']},
 	                                   {id: 'clipart', prefix: null, libs: ['computer', 'finance', 'clipart', 'networking', 'people', 'telco']},
 	                                   {id: 'ibm', prefix: 'ibm', libs: Sidebar.prototype.ibm},
-	                                   {id: 'ibm2'},
+	                                   /*{id: 'ibm2'},*/
 	                                   {id: 'ibm2mondrian'},
 	                                   {id: 'allied_telesis', prefix: 'allied_telesis', libs: Sidebar.prototype.allied_telesis},
 	                                   {id: 'cumulus', libs: ['cumulus']},
@@ -493,7 +493,7 @@
             			          {title: 'Citrix', id: 'citrix', image: IMAGE_PATH + '/sidebar-citrix.png'},
             			          {title: 'Google Cloud Platform', id: 'gcp2', image: IMAGE_PATH + '/sidebar-gcp2.png'},
 					  {title: 'IBM 2.0 Mondrian', id: 'ibm2mondrian', image: IMAGE_PATH + '/sidebar-ibm.png'},
-            			          {title: 'IBM 2.0', id: 'ibm2', image: IMAGE_PATH + '/sidebar-ibm2.png'},
+            			          /*{title: 'IBM 2.0', id: 'ibm2', image: IMAGE_PATH + '/sidebar-ibm2.png'},*/
 					  {title: 'IBM 1.0', id: 'ibm', image: IMAGE_PATH + '/sidebar-ibm.png'},
             			          {title: 'Kubernetes', id: 'kubernetes', image: IMAGE_PATH + '/sidebar-kubernetes.png'},
             			          {title: 'Network', id: 'network', image: IMAGE_PATH + '/sidebar-network.png'},
