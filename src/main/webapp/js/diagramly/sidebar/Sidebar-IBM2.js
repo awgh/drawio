cc24bf72bf4247b9d897373dbb521e5ee23b32a0
jaywarfield <jay.warfield@us.ibm.com>
2021-01-10 18:26:18 -0500

    Sidebar test

--- src/main/webapp/js/diagramly/sidebar/Sidebar-IBM2.js
+++ src/main/webapp/js/diagramly/sidebar/Sidebar-IBM2.js
@@ -195,8 +195,6 @@
 	   		return sb.createVertexTemplateFromCells([bg], bg.geometry.width, bg.geometry.height, name);
 		};
 
-		//this.setCurrentSearchEntryLibrary('ibm2', 'ibm2Mondrian');
-
 		stencils.forEach((section, stencil_index) => {
 			var header = '';
 			var entries = [];
@@ -209,45 +207,15 @@
 					}
 					else
 					{
-						entries.push(this.addEntry(dt + 'tn', function() { return createVertex(stencil); }))
+						entries.push(this.addEntry(dt + stencil_index.toString() + ' ' + section_index.toString(), function() { return createVertex(stencil); }))
 						//entries.push(this.addEntry(dt + 'tn', function() { return createVertex(stencil); }))
 					}
 				});
 				this.setCurrentSearchEntryLibrary('ibm2', 'ibm2' + header.replace(/\s/g,''));
-				this.addPaletteFunctions('ibm2', 'IBM 2.0 /I/ ' + header, false, entries);
+				this.addPaletteFunctions('ibm2', 'IBM 2.0 /J/ ' + header, false, entries);
 			}
 		});
 
 		this.setCurrentSearchEntryLibrary();
-
-		//entries.push(this.addEntry(dt + 'tn', function() { return createVertex('Mondrian', actor_type, '', '') }));
-
-		//this.addPaletteFunctions('ibm2', 'IBM 2.0 /5/ Mondrian', false, entries);
-		
-  		/*		
-		this.addPaletteFunctions('ibm2', 'IBM 2.0 /3/ Mondrian', false,
-		[
-			this.addEntry(dt + 'tn', function()
-		   	{
-				var default_icon = '';
-				var shape_type = 'shapeType=pn';
-				var other_label = 'metaEdit=0;strokeWidth=1';
-				var style_text = 'html=1;whiteSpace=wrap;fontFamily=IBM Plex Sans;fontColor=#000000;fontSize=14;verticalAlign=middle;align=left;spacing=8;spacingLeft=12;spacingRight=16;spacingTop=0;spacingBottom=0'
-			    var bg = new mxCell('', 
-			    		new mxGeometry(0, 0, w, h), "shape=" + gn + ".base" + ";" + shape_type + ";" + style_text + ";" + other_label + ";" + "image=" + default_icon);
-		    	bg.vertex = true;
-		    	bg.setValue(mxUtils.createXmlDocument().createElement('object'));
-				bg.setAttribute('placeholders', '1');
-				bg.setAttribute('label', '<B>%Element-Name%</B><BR><font style=\'font-size: 12px\'>%Element-ID%</font>');
-                bg.setAttribute('Element-ID', 'Element-ID');
-				bg.setAttribute('Element-Name', 'Element-Name');
-				bg.setAttribute('Icon-Name', '');
-			    
-			   	return sb.createVertexTemplateFromCells([bg], bg.geometry.width, bg.geometry.height, 'BASE');
-			})				
-		]);
-		*/
-		
-		//this.setCurrentSearchEntryLibrary();
 	};
 })();
