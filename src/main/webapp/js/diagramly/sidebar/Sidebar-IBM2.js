70aab4f7f4a9537f8f25ca1830c647bc2a6437fb
jaywarfield <jay.warfield@us.ibm.com>
2021-01-13 10:41:20 -0500

    Remove bold type

--- src/main/webapp/js/diagramly/sidebar/Sidebar-IBM2.js
+++ src/main/webapp/js/diagramly/sidebar/Sidebar-IBM2.js
@@ -152,17 +152,17 @@
 				shapeLayout = 'expanded';				
 				if (spanning_groups.includes(shapeName))
 				{
-					extraStyle = ';container=0;collapsible=0;recursiveResize=0;colorFamily=' + shapeColor;
+					extraStyle = ';container=0;collapsible=0;recursiveResize=0;fontStyle=0;colorFamily=' + shapeColor;
 				}
 				else 
 				{
-					extraStyle = ';container=1;collapsible=0;recursiveResize=0;colorFamily=' + shapeColor;
+					extraStyle = ';container=1;collapsible=0;recursiveResize=0;fontStyle=0;colorFamily=' + shapeColor;
 				}
 			}
 			else
 			{
 				shapeLayout = 'collapsed';				
-				extraStyle = ';colorFamily=' + shapeColor;
+				extraStyle = ';fontStyle=0;colorFamily=' + shapeColor;
 			}
 
 			var bg = Sidebar.prototype.addIBM2MondrianVertexTemplateFactoryPlus(shapeType, shapeLayout, extraStyle, shapeId, shapeName, iconName);
